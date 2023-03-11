e601e5059353f7ec41e6b12572786913b4dbc1b6
Yan <yanyakovtsev@gmail.com>
2023-03-11 13:51:40 +0300

    add graceful shutdown

--- cmd/game-tracker/main.go
+++ cmd/game-tracker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"github.com/HeadGardener/game-tracker/configs"
@@ -9,11 +10,17 @@ import (
 	"github.com/HeadGardener/game-tracker/internal/app/services"
 	"github.com/HeadGardener/game-tracker/internal/pkg/server"
 	"go.uber.org/zap"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var confPath = flag.String("conf-path", "./configs/.env", "path to config env")
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	logger, _ := zap.NewProduction()
 
 	dbconfig, err := configs.NewDBConfig(*confPath)
@@ -35,9 +42,28 @@ func main() {
 		logger.Fatal(fmt.Sprintf("unable to read config file, error:%d", err))
 	}
 
-	srv := server.Server{}
-	if err := srv.Run(srvconfig.ServerPort, handler.InitRoutes()); err != nil {
-		// change later
-		logger.Fatal(fmt.Sprintf("error occurring while running server, err:%d", err))
+	srv := &server.Server{}
+
+	go func() {
+		if err := srv.Run(srvconfig.ServerPort, handler.InitRoutes()); err != nil {
+			logger.Error(fmt.Sprintf("error occurring while running server, err:%d", err))
+		}
+	}()
+
+	logger.Info("server start working")
+	<-ctx.Done()
+
+	stop()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Error(fmt.Sprintf("server forced to shutdown: %d", err))
 	}
+
+	if err := dbConn.Close(ctx); err != nil {
+		logger.Error(fmt.Sprintf("db connection forced to shutdown: %d", err))
+	}
+
+	logger.Info("server exiting")
 }
